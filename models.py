10f8e593ae254ba778e439b158085c9cad1b0cbf
MatteoDeMoor <125797081+MatteoDeMoor@users.noreply.github.com>
2025-06-20 02:00:15 +0200

    Update model.py code

--- models.py
+++ models.py
@@ -1,56 +1,52 @@
 import os
 from transformers import pipeline
 
-# Prevent excessive threading overhead in tokenizers
+# prevent tokenisers from spawning too many threads
 os.environ["TOKENIZERS_PARALLELISM"] = "false"
 
-# Constants
-DEVICE = -1  # CPU-only, change to 0 for GPU
+# constants
+DEVICE     = -1
 BATCH_SIZE = 64
 
+# Functions to create pipelines
+pipeline_constructors = {
+    "distilbert": lambda: pipeline(
+        "sentiment-analysis",
+        model="distilbert-base-uncased-finetuned-sst-2-english",
+        device=DEVICE,
+        batch_size=BATCH_SIZE
+    ),
+    "roberta_large": lambda: pipeline(
+        "sentiment-analysis",
+        model="siebert/sentiment-roberta-large-english",
+        device=DEVICE,
+        batch_size=BATCH_SIZE
+    ),
+    "multilingual": lambda: pipeline(
+        "sentiment-analysis",
+        model="nlptown/bert-base-multilingual-uncased-sentiment",
+        device=DEVICE,
+        batch_size=BATCH_SIZE
+    ),
+    "textattack_bert": lambda: pipeline(
+        "sentiment-analysis",
+        model="textattack/bert-base-uncased-SST-2",
+        device=DEVICE,
+        batch_size=BATCH_SIZE
+    ),
+    "textattack_roberta": lambda: pipeline(
+        "sentiment-analysis",
+        model="textattack/roberta-base-SST-2",
+        device=DEVICE,
+        batch_size=BATCH_SIZE
+    ),
+    "twitter_roberta": lambda: pipeline(
+        "sentiment-analysis",
+        model="cardiffnlp/twitter-roberta-base-sentiment",
+        device=DEVICE,
+        batch_size=BATCH_SIZE
+    ),
+}
 
-def load_pipelines():
-    # Load and return a dictionary of sentiment-analysis pipelines
-    # Keys are model identifiers, values are HuggingFace pipeline objects
-    return {
-        "distilbert": pipeline(
-            "sentiment-analysis",
-            model="distilbert-base-uncased-finetuned-sst-2-english",
-            device=DEVICE,
-            batch_size=BATCH_SIZE
-        ),
-        "roberta_large": pipeline(
-            "sentiment-analysis",
-            model="siebert/sentiment-roberta-large-english",
-            device=DEVICE,
-            batch_size=BATCH_SIZE
-        ),
-        "multilingual": pipeline(
-            "sentiment-analysis",
-            model="nlptown/bert-base-multilingual-uncased-sentiment",
-            device=DEVICE,
-            batch_size=BATCH_SIZE
-        ),
-        "textattack_bert": pipeline(
-            "sentiment-analysis",
-            model="textattack/bert-base-uncased-SST-2",
-            device=DEVICE,
-            batch_size=BATCH_SIZE
-        ),
-        "textattack_roberta": pipeline(
-            "sentiment-analysis",
-            model="textattack/roberta-base-SST-2",
-            device=DEVICE,
-            batch_size=BATCH_SIZE
-        ),
-        "twitter_roberta": pipeline(
-            "sentiment-analysis",
-            model="cardiffnlp/twitter-roberta-base-sentiment",
-            device=DEVICE,
-            batch_size=BATCH_SIZE
-        ),
-    }
-
-# Load pipelines once at import time
-df = None
-pipelines = load_pipelines()
+# This dict will be filled in app_api.py during startup
+pipelines = {}
